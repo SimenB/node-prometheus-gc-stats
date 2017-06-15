5d00e1ebc2b4e0460159f6dec93c4c87fa23ab32
Simen Bekkhus <sbekkhus91@gmail.com>
2017-06-15 10:49:59 +0200

    Receive a registry as an argument

--- gc-metrics.js
+++ gc-metrics.js
@@ -1,7 +1,12 @@
 // Credits go to @tcolgate
 
 import Counter from 'prom-client/lib/counter';
+import { version } from 'prom-client/package.json';
 import optional from 'optional';
+
+const promMajor = Number(version.split('.')[0]);
+
+const promSupportsRegistries = promMajor >= 9;
 
 const gc = optional('gc-stats');
 
@@ -17,16 +22,55 @@ const gcTypes = {
 
 const noop = () => {};
 
-export default () => {
+export default registry => {
   if (typeof gc !== 'function') {
     return noop;
   }
 
-  const gcCount = new Counter('nodejs_gc_runs_total', 'Count of total garbage collections.', ['gctype']);
-  const gcTimeCount = new Counter('nodejs_gc_pause_seconds_total', 'Time spent in GC Pause in seconds.', ['gctype']);
-  const gcReclaimedCount = new Counter('nodejs_gc_reclaimed_bytes_total', 'Total number of bytes reclaimed by GC.', [
-    'gctype',
-  ]);
+  let gcCount;
+  let gcTimeCount;
+  let gcReclaimedCount;
+
+  if (registry) {
+    if (!promSupportsRegistries) {
+      throw new Error(
+        "You've provided a registry, but your version of prom-client is too old. Please use prom-client@9 or higher"
+      );
+    }
+
+    const registers = [registry];
+    const labelNames = ['gctype'];
+
+    gcCount = new Counter({
+      name: 'nodejs_gc_runs_total',
+      help: 'Count of total garbage collections.',
+      labelNames,
+      registers,
+    });
+    gcTimeCount = new Counter({
+      name: 'nodejs_gc_pause_seconds_total',
+      help: 'Time spent in GC Pause in seconds.',
+      labelNames,
+      registers,
+    });
+    gcReclaimedCount = new Counter({
+      name: 'nodejs_gc_reclaimed_bytes_total',
+      help: 'Total number of bytes reclaimed by GC.',
+      labelNames,
+      registers,
+    });
+  } else {
+    gcCount = new Counter(
+      'nodejs_gc_runs_total',
+      'Count of total garbage collections.',
+      ['gctype'],
+      registry && [registry]
+    );
+    gcTimeCount = new Counter('nodejs_gc_pause_seconds_total', 'Time spent in GC Pause in seconds.', ['gctype']);
+    gcReclaimedCount = new Counter('nodejs_gc_reclaimed_bytes_total', 'Total number of bytes reclaimed by GC.', [
+      'gctype',
+    ]);
+  }
 
   let started = false;
 
