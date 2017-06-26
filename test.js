688a62a4fc9f988448111436502a7f41646cd117
Simen Bekkhus <sbekkhus91@gmail.com>
2017-06-26 09:04:06 +0200

    Add test

---
+++ test.js
@@ -0,0 +1,40 @@
+/* eslint-env jest*/
+
+import promRegister from 'prom-client/lib/register';
+import PromRegistry from 'prom-client/lib/registry';
+import { version as promVersion } from 'prom-client/package.json';
+import gcMetrics from './gc-metrics';
+
+const promMajor = Number(promVersion.split('.')[0]);
+
+const promSupportsRegistries = promMajor >= 9;
+
+jest.mock('gc-stats');
+
+afterEach(() => {
+  promRegister.clear();
+});
+
+test('should register metrics', () => {
+  expect(promRegister.getMetricsAsJSON()).toHaveLength(0);
+
+  gcMetrics();
+
+  expect(promRegister.getMetricsAsJSON()).toHaveLength(3);
+});
+
+test('should register metrics to custom register', () => {
+  if (!promSupportsRegistries) {
+    return;
+  }
+
+  const register = new PromRegistry();
+
+  expect(promRegister.getMetricsAsJSON()).toHaveLength(0);
+  expect(register.getMetricsAsJSON()).toHaveLength(0);
+
+  gcMetrics(register);
+
+  expect(promRegister.getMetricsAsJSON()).toHaveLength(0);
+  expect(register.getMetricsAsJSON()).toHaveLength(3);
+});
