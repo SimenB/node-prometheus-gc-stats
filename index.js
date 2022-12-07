9977181cfc0975b22194d664be860648bf9f4b3e
David Dios <david.dios@loveholidays.com>
2022-12-07 07:30:44 +0000

    feat: add support for v8 version 10+ (#83)

--- index.js
+++ index.js
@@ -7,15 +7,31 @@ const optional = require('optional');
 
 const gc = optional('gc-stats');
 
-const gcTypes = {
-  0: 'Unknown',
-  1: 'Scavenge',
-  2: 'MarkSweepCompact',
-  3: 'ScavengeAndMarkSweepCompact',
-  4: 'IncrementalMarking',
-  8: 'WeakPhantom',
-  15: 'All',
-};
+const v8Version = process.versions.v8;
+const v8MajorVersion = Number(v8Version.split('.')[0]);
+
+const gcTypes =
+  v8MajorVersion < 10
+    ? {
+        // https://github.com/nodejs/node/blob/554fa24916c5c6d052b51c5cee9556b76489b3f7/deps/v8/include/v8.h#L6137-L6144
+        0: 'Unknown',
+        1: 'Scavenge',
+        2: 'MarkSweepCompact',
+        3: 'ScavengeAndMarkSweepCompact',
+        4: 'IncrementalMarking',
+        8: 'WeakPhantom',
+        15: 'All',
+      }
+    : {
+        // https://github.com/nodejs/node/blob/ccd3a42dd9ea2132111610e667ee338618e3b101/deps/v8/include/v8-callbacks.h#L150-L159
+        0: 'Unknown',
+        1: 'Scavenge',
+        2: 'MinorMarkCompact',
+        4: 'MarkSweepCompact',
+        8: 'IncrementalMarking',
+        16: 'ProcessWeakCallbacks',
+        31: 'All',
+      };
 
 const noop = () => {};
 
